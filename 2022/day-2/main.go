f19d8da98c63630bec9cc176a5d3273ec8e413ef
Stian Frøystein <stianfroy@gmail.com>
2022-12-02 15:18:00 +0900

    feat(day-2): implement main logic with example input

--- 2022/day-2/main.go
+++ 2022/day-2/main.go
@@ -1,5 +1,91 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+// Rock > Scissors
+// Scissors > Paper
+// Paper > Rock
+
+// A/X = Rock     (1)
+// B/Y = Paper    (2)
+// C/Z = Scissors (3)
+
+// Lose = 0
+// Draw = 3
+// Won  = 6
+
+var result string
+var score int
+
+func rockPaperScissors(opponent string, player string) (result string, score int) {
+	switch opponent {
+	case "A":
+		switch player {
+		case "X":
+			// Draw
+			score = 3 + 1
+			result = "Draw"
+		case "Y":
+			// Win
+			score = 6 + 2
+			result = "Player wins, opponent loses"
+		case "Z":
+			// Lose
+			score = 0 + 3
+			result = "Player loses, opponent wins"
+		}
+	case "B":
+		switch player {
+		case "X":
+			// Lose
+			score = 0 + 1
+			result = "Player loses, opponent wins"
+		case "Y":
+			// Draw
+			score = 3 + 2
+			result = "Draw"
+		case "Z":
+			// Win
+			score = 6 + 3
+			result = "Player wins, opponent loses"
+		}
+	case "C":
+		switch player {
+		case "X":
+			// Win
+			score = 6 + 1
+			result = "Player wins, opponent loses"
+		case "Y":
+			// Lose
+			score = 0 + 2
+			result = "Player loses, opponent wins"
+		case "Z":
+			// Draw
+			score = 3 + 3
+			result = "Draw"
+		}
+	}
+
+	return result, score
+}
+
+func main() {
+	totalScore := 0
+
+	input := [][]string{
+		{"A", "Y"},
+		{"B", "X"},
+		{"C", "Z"},
+	}
+
+	for i := range input {
+		result, score = rockPaperScissors(input[i][0], input[i][1])
+		fmt.Println(result+". Score:", score)
+
+		totalScore += score
+	}
+
+	fmt.Println("Total score:", totalScore)
 }
