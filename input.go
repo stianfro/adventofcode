1770ad7b5ee19052146d774a2689bb23f823d15f
Stian Frøystein <stianfroy@gmail.com>
2022-12-03 15:21:03 +0900

    feat(input): try to make script for getting input

---
+++ input.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	session, _ := os.ReadFile("~/.config/aocd/token")
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "https://adventofcode.com/2022/day/"+os.Args[1]+"/input", nil)
+	req.Header.Set("Cookie", string(session))
+	res, _ := client.Do(req)
+
+	bodyBytes, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(bodyBytes))
+}
