84181f53340874801d508c722d8a755456fc1b1a
Stian Frøystein <stianfroy@gmail.com>
2022-12-05 15:43:31 +0900

    feat(day-5): don't use structs for this

--- 2022/day-5/main.go
+++ 2022/day-5/main.go
@@ -1,54 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-type Crate struct {
-	Stack1 []string
-	Stack2 []string
-	Stack3 []string
+func printStacks(stacks [][]string) {
+	for _, v := range stacks {
+		fmt.Println(v)
+	}
 }
 
-// type Crate struct {
-// 	Stack1 []string
-// 	Stack2 []string
-// 	Stack3 []string
-// 	Stack4 []string
-// 	Stack5 []string
-// 	Stack6 []string
-// 	Stack7 []string
-// 	Stack8 []string
-// 	Stack9 []string
-// }
-
-func printCrate(crate Crate) {
-	fmt.Println(crate.Stack1)
-	fmt.Println(crate.Stack2)
-	fmt.Println(crate.Stack3)
-	// fmt.Println(crate.Stack4)
-	// fmt.Println(crate.Stack5)
-	// fmt.Println(crate.Stack6)
-	// fmt.Println(crate.Stack7)
-	// fmt.Println(crate.Stack8)
-	// fmt.Println(crate.Stack9)
+func printStacksTop(stacks [][]string) {
+	for _, v := range stacks {
+		fmt.Println(v[len(v)-1])
+	}
 }
 
 func main() {
-	input := Crate{
-		Stack1: []string{"Z", "N"},
-		Stack2: []string{"D", "C", "M"},
-		Stack3: []string{"P"},
+	stacks := [][]string{
+		{"Z", "N"},
+		{"M", "C", "D"},
+		{"P"},
 	}
-	// input := Crate{
-	// 	Stack1: []string{"S", "L", "W"},
-	// 	Stack2: []string{"J", "T", "N", "Q"},
-	// 	Stack3: []string{"S", "C", "H", "F", "J"},
-	// 	Stack4: []string{"T", "R", "M", "W", "N", "G", "B"},
-	// 	Stack5: []string{"T", "R", "L", "S", "D", "H", "Q", "B"},
-	// 	Stack6: []string{"M", "J", "B", "V", "F", "H", "R", "L"},
-	// 	Stack7: []string{"D", "W", "R", "N", "J", "M"},
-	// 	Stack8: []string{"B", "Z", "T", "F", "H", "N", "D", "J"},
-	// 	Stack9: []string{"H", "L", "Q", "N", "B", "F", "T"},
+
+	// stacks := [][]string{
+	// 	{"S", "L", "W"},
+	// 	{"J", "T", "N", "Q"},
+	// 	{"S", "C", "H", "F", "J"},
+	// 	{"T", "R", "M", "W", "N", "G", "B"},
+	// 	{"T", "R", "L", "S", "D", "H", "Q", "B"},
+	// 	{"M", "J", "B", "V", "F", "H", "R", "L"},
+	// 	{"D", "W", "R", "N", "J", "M"},
+	// 	{"B", "Z", "T", "F", "H", "N", "D", "J"},
+	// 	{"H", "L", "Q", "N", "B", "F", "T"},
 	// }
 
-	printCrate(input)
+	I := []string{
+		"move 1 from 2 to 1",
+		"move 3 from 1 to 3",
+		"move 2 from 2 to 1",
+		"move 1 from 1 to 2",
+	}
+
+	for _, i := range I {
+		instruction := strings.Split(i, " ")
+
+		mov, _ := strconv.Atoi(instruction[1])
+		src, _ := strconv.Atoi(instruction[3])
+		dst, _ := strconv.Atoi(instruction[5])
+
+		fmt.Println(mov, src, dst)
+	}
+
+	printStacks(stacks)
+	printStacksTop(stacks)
 }
