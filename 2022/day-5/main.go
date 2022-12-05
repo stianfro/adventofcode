7de884235691ac3f686e81c2da92774fc555f5cc
Stian Frøystein <stianfroy@gmail.com>
2022-12-05 16:20:58 +0900

    feat(day-5): part one complete

--- 2022/day-5/main.go
+++ 2022/day-5/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
+func getInput(fileName string) []string {
+	inputBytes, err := os.ReadFile(fileName)
+	if err != nil {
+		println("failed to read input")
+	}
+
+	return strings.Split(string(inputBytes), "\n")
+}
+
 func printStacks(stacks [][]string) {
-	for _, v := range stacks {
-		fmt.Println(v)
+	for i, v := range stacks {
+		fmt.Println(i+1, v)
 	}
 }
 
@@ -21,44 +31,44 @@ func printStacksTop(stacks [][]string) string {
 }
 
 func main() {
-	stacks := [][]string{
-		{"Z", "N"},
-		{"M", "C", "D"},
-		{"P"},
-	}
-
 	// stacks := [][]string{
-	// 	{"S", "L", "W"},
-	// 	{"J", "T", "N", "Q"},
-	// 	{"S", "C", "H", "F", "J"},
-	// 	{"T", "R", "M", "W", "N", "G", "B"},
-	// 	{"T", "R", "L", "S", "D", "H", "Q", "B"},
-	// 	{"M", "J", "B", "V", "F", "H", "R", "L"},
-	// 	{"D", "W", "R", "N", "J", "M"},
-	// 	{"B", "Z", "T", "F", "H", "N", "D", "J"},
-	// 	{"H", "L", "Q", "N", "B", "F", "T"},
+	// 	{"Z", "N"},
+	// 	{"M", "C", "D"},
+	// 	{"P"},
 	// }
 
-	I := []string{
-		"move 1 from 2 to 1",
-		"move 3 from 1 to 3",
-		"move 2 from 2 to 1",
-		"move 1 from 1 to 2",
+	stacks := [][]string{
+		{"S", "L", "W"},
+		{"J", "T", "N", "Q"},
+		{"S", "C", "H", "F", "J"},
+		{"T", "R", "M", "W", "N", "G", "B"},
+		{"T", "R", "L", "S", "D", "H", "Q", "B"},
+		{"M", "J", "B", "V", "F", "H", "R", "L"},
+		{"D", "W", "R", "N", "J", "M"},
+		{"B", "Z", "T", "F", "H", "N", "D", "J"},
+		{"H", "L", "Q", "N", "B", "F", "T"},
 	}
 
-	for _, i := range I {
+	fmt.Println("--- Part One ---")
+	printStacks(stacks)
+	fmt.Println("")
+
+	fmt.Println("Start moving crates:")
+	for _, i := range getInput("input.txt") {
 		instruction := strings.Split(i, " ")
 
 		mov, _ := strconv.Atoi(instruction[1])
 		src, _ := strconv.Atoi(instruction[3])
 		dst, _ := strconv.Atoi(instruction[5])
 
+		fmt.Println(i)
+
 		for i := 0; i < mov; i++ {
-			fmt.Println("")
 			stacks[dst-1] = append(stacks[dst-1], stacks[src-1][len(stacks[src-1])-1])
 			stacks[src-1] = stacks[src-1][:len(stacks[src-1])-1]
-			printStacks(stacks)
 		}
+		printStacks(stacks)
+		fmt.Println("")
 	}
 
 	p1 := printStacksTop(stacks)
