167ec14c96111c607c0ddf71991b850689ab82a6
Stian Frøystein <stianfroy@gmail.com>
2022-12-09 21:39:25 +0900

    feat(day-9): construct grid from motions

--- 2022/day-9/main.go
+++ 2022/day-9/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
-var headPosition Coordinate
-var tailPosition Coordinate
+// var xMax []int
+// var yMax []int
 
-type Coordinate struct {
-	X int
-	Y int
-}
+// type Coordinate struct {
+// 		X int
+// 		Y int
+// }
+
+// var headPosition Coordinate
+// var tailPosition Coordinate
+
+var xMotions []int
+var yMotions []int
 
 func getInput(fileName string) []string {
 	inputBytes, err := os.ReadFile(fileName)
@@ -22,11 +31,54 @@ func getInput(fileName string) []string {
 }
 
 func generateGrid(motions []string) {
+	x := 1
+	y := 1
 
+	for _, v := range motions {
+		m := strings.Split(v, " ")
+
+		dir := m[0]
+		mov, _ := strconv.Atoi(m[1])
+
+		switch dir {
+		case "R":
+			x += mov
+			xMotions = append(xMotions, x)
+		case "L":
+			x -= mov
+			xMotions = append(xMotions, x)
+		case "U":
+			y += mov
+			yMotions = append(yMotions, y)
+		case "D":
+			y -= mov
+			yMotions = append(yMotions, y)
+		}
+	}
+
+	sort.Ints(xMotions)
+	sort.Ints(yMotions)
+
+	xMax := xMotions[len(xMotions)-1]
+	yMax := yMotions[len(yMotions)-1]
+
+	fmt.Println(xMax, yMax)
 }
 
 func main() {
 	input := getInput("input-example.txt")
 
 	generateGrid(input)
+
+	grid := [][]string{
+		{".", ".", ".", ".", ".", "."}, // grid[0]
+		{".", ".", ".", ".", ".", "."}, // grid[1]
+		{".", ".", ".", ".", ".", "."}, // grid[2]
+		{".", ".", ".", ".", ".", "."}, // grid[3]
+		{"H", ".", ".", ".", ".", "."}, // grid[4]
+	}
+
+	for _, row := range grid {
+		fmt.Println(row)
+	}
 }
