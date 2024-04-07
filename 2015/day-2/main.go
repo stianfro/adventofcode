eb2dde10f067999029ced6b81be27e68878093cf
Stian Frøystein <stianfroy@gmail.com>
2024-04-07 20:11:29 +0900

    feat(2015): complete d2p2

--- 2015/day-2/main.go
+++ 2015/day-2/main.go
@@ -8,21 +8,52 @@ import (
 	"strings"
 )
 
-// var input = []string{
-// 	"2x3x4",
-// 	"1x1x10",
-// }
-
 func main() {
-	var wrappingPaperTotal int
-
 	input, err := os.ReadFile("input.txt")
 	if err != nil {
 		log.Fatalln("error reading")
 	}
 	data := strings.Split(string(input), "\n")
+	// var data = []string{
+	// 	"2x3x4",
+	// 	"1x1x10",
+	// }
 
-	for i, box := range data {
+	partOne(data)
+	partTwo(data)
+}
+
+func partTwo(boxes []string) {
+	var ribbonTotal int
+
+	for _, box := range boxes {
+		if len(box) <= 0 {
+			continue
+		}
+
+		lwh := strings.Split(box, "x")
+
+		l, _ := strconv.Atoi(lwh[0])
+		w, _ := strconv.Atoi(lwh[1])
+		h, _ := strconv.Atoi(lwh[2])
+
+		lwhSorted := []int{l, w, h}
+		slices.Sort(lwhSorted)
+
+		volume := l * w * h
+		perimeter := 2*lwhSorted[0] + 2*lwhSorted[1]
+
+		ribbon := perimeter + volume
+		ribbonTotal += ribbon
+	}
+
+	log.Println("p2:", ribbonTotal)
+}
+
+func partOne(boxes []string) {
+	var wrappingPaperTotal int
+
+	for _, box := range boxes {
 		if len(box) <= 0 {
 			continue
 		}
@@ -50,10 +81,8 @@ func main() {
 		smallestSide = sides[0]
 
 		wrappingPaper = 2*sideLW + 2*sideWH + 2*sideHL + smallestSide
-		log.Println(i, wrappingPaper)
-
 		wrappingPaperTotal += wrappingPaper
 	}
 
-	log.Println("total:", wrappingPaperTotal)
+	log.Println("p1:", wrappingPaperTotal)
 }
