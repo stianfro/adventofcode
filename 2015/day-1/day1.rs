5dccc8742699aebcf8ae6dd4690a9dd21f7a3bfc
Stian Frøystein <stianfroy@gmail.com>
2024-07-17 18:47:58 +0900

    feat(2015): complete day1 part2 with rust

--- 2015/day-1/day1.rs
+++ 2015/day-1/day1.rs
@@ -7,17 +7,40 @@ fn read_input(file_path: &str) -> io::Result<String> {
 fn main() -> io::Result<()> {
     let directions = read_input("input.txt")?;
     let directions_slice: Vec<char> = directions.chars().collect();
-    let mut current_position = 0;
 
-    for dir in directions_slice {
-        if dir == '(' {
-            current_position += 1;
-        } else if dir == ')' {
-            current_position -= 1;
+    part_one(directions_slice.clone());
+    part_two(directions_slice.clone());
+
+    Ok(())
+}
+
+fn part_one(directions: Vec<char>) {
+    let mut current_floor = 0;
+
+    for direction in directions {
+        if direction == '(' {
+            current_floor += 1;
+        } else if direction == ')' {
+            current_floor -= 1;
         }
     }
 
-    println!("answer: {}", current_position);
+    println!("part one: {}", current_floor);
+}
 
-    Ok(())
+fn part_two(directions: Vec<char>) {
+    let mut current_floor = 0;
+
+    for (index, direction) in directions.iter().enumerate() {
+        if *direction == '(' {
+            current_floor += 1;
+        } else if *direction == ')' {
+            current_floor -= 1;
+        }
+
+        if current_floor == -1 {
+            println!("part two: {}", index + 1);
+            break;
+        }
+    }
 }
