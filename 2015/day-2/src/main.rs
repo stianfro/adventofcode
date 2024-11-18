b43a513fe3638bd8ff69ddc9889f6eacc4bf2a4d
Stian Frøystein <stianfroy@gmail.com>
2024-11-18 09:10:06 +0900

    chore: commit unfinished stuff

--- 2015/day-2/src/main.rs
+++ 2015/day-2/src/main.rs
@@ -5,15 +5,42 @@ fn read_input(file_path: &str) -> io::Result<String> {
 }
 
 fn main() -> io::Result<()> {
-    let input = read_input("input.txt")?;
-    let input_slice: Vec<char> = input.chars().collect();
+    let file_path =
+        "/Users/stianfroystein/src/github.com/stianfro/adventofcode/2015/day-2/input.txt";
+
+    let input = read_input(file_path)?;
+    let input_slice: Vec<&str> = input.split('\n').collect();
 
     part_one(input_slice.clone());
-    part_two(input_slice.clone());
+    // part_two(input_slice.clone());
 
     Ok(())
 }
 
-fn part_one(input: Vec<char>) {}
+fn part_one(presents: Vec<&str>) {
+    let mut wrapping_total = 0;
 
-fn part_two(input: Vec<char>) {}
+    for present in presents {
+        if present.is_empty() {
+            break;
+        }
+
+        let dimensions: Vec<&str> = present.split('x').collect();
+
+        let l = dimensions[0];
+        let w = dimensions[1];
+        let h = dimensions[2];
+
+        let l_int: i32 = l.parse().expect("not a valid number");
+        let w_int: i32 = w.parse().expect("not a valid number");
+        let h_int: i32 = h.parse().expect("not a valid number");
+
+        // TODO: create slice of the sides
+        let wrapping = 2 * l_int * w_int + 2 * w_int * h_int + 2 * h_int * l_int;
+        wrapping_total += wrapping;
+    }
+
+    println!("part one: {wrapping_total}");
+}
+
+// fn part_two(dimensions: Vec<char>) {}
