9f1c5c035df036d12ba5e51c75c9d72cd8576311
Stian Frøystein <stianfroy@gmail.com>
2024-12-04 15:12:35 +0900

    feat(2024/day-2): solve part two
    used hints

--- 2024/day-2/src/main.rs
+++ 2024/day-2/src/main.rs
@@ -10,200 +10,90 @@ fn main() {
     println!("Part 2: {p2}");
 }
 
-// 1 2 3 4 5 <- report
-// ^
-// level
-
 fn part_1(input: &str) -> i32 {
-    let data = input.lines();
+    let lines = input.lines();
 
     let mut safe_reports = 0;
 
-    for report in data {
-        let levels = report.split(' ').collect::<Vec<_>>();
+    for line in lines {
+        let levels = line.split(' ').collect::<Vec<_>>();
+        let mut levels_int: Vec<i32> = Vec::new();
 
-        let mut safe = true;
-
-        for (i, &level) in levels.iter().enumerate() {
-            if level.is_empty() || i == 0 {
-                continue;
-            }
-
-            let curr: i32 = level.parse().unwrap();
-            let prev: i32 = levels[i - 1].parse().unwrap();
-
-            let diff = curr - prev;
-
-            let mut diff_prev: i32 = 0;
-
-            if i >= 2 {
-                let prev_prev: i32 = levels[i - 2].parse().unwrap();
-
-                diff_prev = prev - prev_prev
-            }
-
-            if !within_limits(diff) {
-                safe = false;
-            }
-
-            if !same_direction(diff, diff_prev) {
-                safe = false;
-            }
+        for lvl in levels {
+            let lvl_int: i32 = lvl.parse().unwrap();
+            levels_int.push(lvl_int);
         }
 
-        if !safe {
-            continue;
+        if is_safe(levels_int) {
+            safe_reports += 1;
         }
-
-        safe_reports += 1;
     }
 
     safe_reports
+}
+
+fn is_safe(report: Vec<i32>) -> bool {
+    let mut report_sorted = report.clone();
+    let mut report_sorted_reverse = report.clone();
+
+    report_sorted.sort();
+    report_sorted_reverse.sort_by(|a, b| b.cmp(a));
+
+    let increasing_or_decreasing = report == report_sorted || report == report_sorted_reverse;
+    let mut safe = true;
+
+    for (i, _) in report.iter().enumerate() {
+        if i + 1 == report.len() {
+            continue;
+        }
+
+        let diff = (report[i] - report[i + 1]).abs();
+
+        if !(1..=3).contains(&diff) {
+            safe = false
+        }
+    }
+
+    increasing_or_decreasing && safe
 }
 
 fn part_2(input: &str) -> i32 {
-    let data = input.lines();
+    let lines = input.lines();
 
     let mut safe_reports = 0;
 
-    for report in data {
-        let levels = report.split(' ').collect::<Vec<_>>();
-        let mut levels_r2 = levels.clone();
+    for line in lines {
+        let levels = line.split(' ').collect::<Vec<_>>();
+        let mut report: Vec<i32> = Vec::new();
 
-        let mut unsafe_count = 0;
+        for lvl in levels {
+            report.push(lvl.parse().unwrap());
+        }
 
-        for (i, &level) in levels.iter().enumerate() {
-            if level.is_empty() || i == 0 {
-                continue;
-            }
+        let mut safe = false;
 
-            let curr: i32 = level.parse().unwrap();
-            let prev: i32 = levels[i - 1].parse().unwrap();
+        for (i, _) in report.iter().enumerate() {
+            let mut vec1 = report[0..i].to_vec();
+            let mut vec2 = report[i + 1..report.len()].to_vec();
 
-            let diff = curr - prev;
+            vec1.append(&mut vec2);
 
-            let mut diff_prev: i32 = 0;
-
-            if i >= 2 {
-                let prev_prev: i32 = levels[i - 2].parse().unwrap();
-
-                diff_prev = prev - prev_prev
-            }
-
-            if !within_limits(diff) {
-                if unsafe_count < 1 {
-                    levels_r2.remove(i);
-                }
-
-                unsafe_count += 1
-            }
-
-            if !same_direction(diff, diff_prev) {
-                if unsafe_count < 1 {
-                    if diff >= 0 {
-                        println!("Removing {prev} from '{report}'");
-                        levels_r2.remove(i - 1);
-                    } else {
-                        println!("Removing {curr} from '{report}'");
-                        levels_r2.remove(i);
-                    }
-                }
-
-                unsafe_count += 1
+            if is_safe(vec1) {
+                safe = true
             }
         }
 
-        let mut safe = true;
-
-        for (i, &level) in levels_r2.iter().enumerate() {
-            if level.is_empty() || i == 0 {
-                continue;
-            }
-
-            let curr: i32 = level.parse().unwrap();
-            let prev: i32 = levels_r2[i - 1].parse().unwrap();
-
-            let diff = curr - prev;
-
-            let mut diff_prev: i32 = 0;
-
-            if i >= 2 {
-                let prev_prev: i32 = levels_r2[i - 2].parse().unwrap();
-
-                diff_prev = prev - prev_prev
-            }
-
-            if !within_limits(diff) {
-                safe = false;
-            }
-
-            if !same_direction(diff, diff_prev) {
-                safe = false;
-            }
+        if safe {
+            safe_reports += 1
         }
-
-        if !safe {
-            // println!("Unsafe: {report}");
-            continue;
-        }
-
-        // println!("Safe: {report}");
-
-        safe_reports += 1;
     }
 
     safe_reports
-}
-
-fn same_direction(curr: i32, prev: i32) -> bool {
-    if curr <= 0 && prev <= 0 {
-        return true;
-    }
-
-    if curr >= 0 && prev >= 0 {
-        return true;
-    }
-
-    false
-}
-
-fn within_limits(diff: i32) -> bool {
-    if diff == 0 {
-        return false;
-    }
-
-    if (-3..=3).contains(&diff) {
-        return true;
-    }
-
-    false
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_within_limits() {
-        // ok
-        assert!(within_limits(-3));
-        assert!(within_limits(3));
-
-        // not ok
-        assert!(!within_limits(-5));
-        assert!(!within_limits(-4));
-    }
-
-    #[test]
-    fn test_same_direction() {
-        // ok
-        assert!(same_direction(3, 2));
-        assert!(same_direction(1, 1));
-
-        // not ok
-        assert!(!same_direction(-1, 1));
-        assert!(!same_direction(1, -1));
-    }
 
     #[test]
     fn test_part_1() {
@@ -221,25 +111,17 @@ mod tests {
     }
 
     #[test]
-    fn test_part_2_example() {
+    fn test_part_2() {
         let input = String::from(
-            "7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9",
+            "44 47 50 51 53 54 53
+70 73 75 77 80 81 84 84
+1 3 4 7 10 13 16 20
+47 49 52 53 55 57 60 65
+69 70 71 70 71
+22 23 20 21 24 27 24",
         );
         let result = part_2(&input);
 
         assert_eq!(result, 4)
-    }
-
-    #[test]
-    fn test_part_2_special() {
-        let input = String::from("44 47 50 51 53 54 53");
-        let result = part_2(&input);
-
-        assert_eq!(result, 1)
     }
 }
